624110f6b129f4be6720b32d9a03555579503d14
whoismiranda2 <andreamirandapg@gmail.com>
2025-03-27 02:19:52 -0600

    Update juegoAdivinanza.java
    Se creó la parte que permite al jugador a intentar adivinar el número en la cantidad de intentos establecida (10) y también le indica cuando se ha quedado sin oportunidades para seguir intentando adivinar.

--- juegoAdivinanza.java
+++ juegoAdivinanza.java
@@ -4,7 +4,24 @@ import java.util.Scanner;
 
 public class JuegoAdivinanza {
     public static void main(String[] args) {
+        int intentos = 0;
+        int maxIntentos = 10;
+        boolean adivinado = false;
         
+        System.out.println("¡Bienvenido al juego de adivinanza!");
+        System.out.println("Debes adivinar un número entre 1 y 100.");
+        System.out.println("Tienes 10 intentos. ¡Buena suerte!");
 
- }
+        while(intentos < maxIntentos){
+
+        }
+        if (!adivinado) 
+        System.out.println("Lo siento, has agotado tus intentos."");
+
+        scanner.close();
+
+    }
+}
+
+    }
 }
