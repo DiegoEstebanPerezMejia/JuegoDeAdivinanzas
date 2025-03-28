fc305eb31d97263deeebc329f8ef018b285037a4
AnglAjc <angeladrianjuarezcorona@gmail.com>
2025-03-27 23:27:57 -0600

    Implementacion de la funcion para generar numero

--- juegoAdivinanza.java
+++ juegoAdivinanza.java
@@ -4,7 +4,28 @@ import java.util.Scanner;
 
 public class JuegoAdivinanza {
     public static void main(String[] args) {
-        
+        Random rand = new Random();
+        int numeroAleatorio = rand.nextInt(100) + 1; 
+        Scanner scanner = new Scanner(System.in);
+        int intentos = 0;
+        int maxIntentos = 10;
+        int numeroUsuario = 0;
+        boolean adivinado = false;
+        boolean entradaValida = false;
+
+        System.out.println("¡Bienvenido al juego de adivinanza!");
+        System.out.println("Debes adivinar un número entre 1 y 100.");
+        System.out.println("Tienes 10 intentos. ¡Buena suerte!");
+
+        while(intentos < maxIntentos){
+
+            System.out.println("Intento "+ (intentos+1) +" de "+ maxIntentos +". Introduce un numero: ");
+            
+            if (scanner.hasNextInt()) {
+                numeroUsuario = scanner.nextInt();
+                entradaValida = true;
+            } 
+        }
 
  }
 }
